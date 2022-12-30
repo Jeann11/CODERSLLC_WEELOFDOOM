625cb8e685decf80e71783e82399cf72ef10a829
Ashur19 <iglesiasjota1911@gmail.com>
2022-12-30 09:41:22 +0100

    Third commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-window.alert ("Wheel of Doom is an Alpha. We hope to develop the next sprints soon. Sorry for the inconvenience.");
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
 
 var blink = document.getElementById('blink');
   
